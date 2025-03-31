95cb98a6e48d1b208e98eb475ccb9175efcc85df
GabrielSpdf <gb.spada@hotmail.com>
2025-03-31 15:20:19 -0300

    added: default-terminal on tmux

--- config/tmuxor.sh
+++ config/tmuxor.sh
@@ -5,5 +5,6 @@ set -g status-fg white
 set -g status-left '#[bg=green] #S #[default]'
 set -g status-right '#[fg=yellow] %H:%M %d-%b-%y #[default]'
 set -g default-terminal "tmux-256color"
+set-option -g default-shell /bin/zsh
 set -ag terminal-overrides ",xterm-256color:RGB"
 EOT
